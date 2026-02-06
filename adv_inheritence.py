ce8eb2e9e99938fdef9192993bfe71df06f99325
rahul <rahulkr18499@gmail.com>
2026-02-06 23:38:40 +0530

    oopslecture completed

---
+++ adv_inheritence.py
@@ -0,0 +1,20 @@
+# Single or basic Inheritence 
+# class Parent:
+#     def __init__(self,name):
+#         self.name = name
+#     def greet(self):
+#         print(f"Hello , my name is {self.name}.")
+
+
+
+# # delivered class
+# class Child(Parent):
+#     def play(self):
+#         print(f"{self.name} is playing.")
+
+
+#    # create an instance of child 
+# child = Child("Alice")
+# child.greet()
+# child.play()
+     
